71554980f8d1ff4ed519703bf595afbf57d82e35
Daniel Holmgren <dtholmgren@gmail.com>
2019-08-08 13:27:05 -0500

    added FissionUser subclass for library methods that require auth

--- src/fission.ts
+++ src/fission.ts
@@ -9,26 +9,13 @@ export type CID = string
 
 export default class Fission {
   baseURL: string
-  auth?: { username: string; password: string }
 
   constructor(baseURL?: string) {
     this.baseURL = baseURL || BASE_URL_DEFAULT
   }
 
-  login(username: string, password: string): Fission {
-    if (!username || !password) {
-      throw new Error('Must supply both a username and password on login')
-    }
-    this.auth = { username, password }
-    return this
-  }
-
-  async list(): Promise<CID[]> {
-    if (!this.auth) {
-      throw new Error('Must be logged in to list available CIDs')
-    }
-    const { data } = await axios.get<CID[]>(`${this.baseURL}/ipfs/cids`, { auth: this.auth })
-    return data
+  login(username: string, password: string): FissionUser {
+    return new FissionUser(this.baseURL, username, password)
   }
 
   async content(cid: CID): Promise<Content> {
@@ -40,11 +27,29 @@ export default class Fission {
   url(cid: CID): string {
     return `${this.baseURL}/ipfs/${cid}`
   }
+}
+
+export class FissionUser extends Fission {
+  auth: {
+    username: string
+    password: string
+  }
+
+  constructor(baseURL: string, username: string, password: string) {
+    if (!username || !password) {
+      throw new Error('Must supply both a username and password on login')
+    }
+    super(baseURL)
+    this.auth = { username, password }
+    return this
+  }
+
+  async list(): Promise<CID[]> {
+    const { data } = await axios.get<CID[]>(`${this.baseURL}/ipfs/cids`, { auth: this.auth })
+    return data
+  }
 
   async add(content: Content, name?: string): Promise<CID> {
-    if (!this.auth) {
-      throw new Error('Must be logged in to add content to IPFS')
-    }
     const headers = { 'content-type': 'application/octet-stream' }
     const nameStr = name ? `?name=${name}` : ''
     const { data } = await axios.post<CID>(`${this.baseURL}/ipfs${nameStr}`, content, {
@@ -55,16 +60,10 @@ export default class Fission {
   }
 
   async remove(cid: CID) {
-    if (!this.auth) {
-      throw new Error('Must be logged in to remove content from IPFs')
-    }
     await axios.delete(`${this.baseURL}/ipfs/${cid}`, { auth: this.auth })
   }
 
   async pin(cid: CID) {
-    if (!this.auth) {
-      throw new Error('Must be logged in to pin content')
-    }
     await axios.put(`${this.baseURL}/ipfs/${cid}`, {}, { auth: this.auth })
   }
 }
