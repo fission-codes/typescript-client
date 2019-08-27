a828c2c262d5cb0ee864e72336ab111f6e337913
Daniel Holmgren <dtholmgren@gmail.com>
2019-08-27 12:43:37 -0500

    added additional tests for branches

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,4 +1,15 @@
-import Fission, { FissionUser, CID, Content as IPFSContent } from '../src/client'
+import Fission, {
+  FissionUser,
+  content,
+  url,
+  cids,
+  add,
+  remove,
+  pin,
+  CID,
+  Auth,
+  Content as IPFSContent
+} from '../src/client'
 import { Object as JSONObject } from 'json-typescript'
 import axios from 'axios'
 const sinon = require('sinon')
@@ -7,6 +18,7 @@ const sinon = require('sinon')
 const baseURL = 'https://hostless.dev'
 const username = 'test_username'
 const password = 'test_password'
+const auth = { username, password } as Auth
 
 const strContent = 'string content'
 const jsonContent = {
@@ -35,9 +47,9 @@ describe('Fission', () => {
     expect(url).toEqual(`${baseURL}/ipfs/${testCID}`)
   })
 
-  it('defaults baseURL to https://hostless.dev', () => {
-    const url = new Fission().url(testCID)
-    expect(url).toEqual(`https://hostless.dev/ipfs/${testCID}`)
+  it('defaults baseURL when formatting url', () => {
+    const contentURL = url(testCID)
+    expect(contentURL).toEqual(`https://hostless.dev/ipfs/${testCID}`)
   })
 
   describe('retrieves IPFS content', () => {
@@ -48,14 +60,15 @@ describe('Fission', () => {
       fake = sinon.fake.returns(new Promise(r => r({ data: strContent })))
       sinon.replace(axios, 'get', fake)
       ipfsContent = await fission.content(testCID)
+      await content(testCID)
     })
 
     it('returns IPFS content', () => {
       expect(ipfsContent).toEqual(strContent)
     })
 
-    it('sends one GET request', () => {
-      expect(fake.callCount).toEqual(1)
+    it('sends one GET request per call', () => {
+      expect(fake.callCount).toEqual(2)
     })
 
     it('sends request with a properly formatted url', () => {
@@ -68,6 +81,10 @@ describe('Fission', () => {
           'content-type': 'application/octet-stream'
         }
       })
+    })
+
+    it('defaults baseURL to https://hostless.dev', () => {
+      expect(fake.args[1][0]).toEqual(`https://hostless.dev/ipfs/${testCID}`)
     })
   })
 })
@@ -83,7 +100,7 @@ describe('FissionUser', () => {
 
   describe('returns cids associated with user', () => {
     let fake: sinon.SinonSpy
-    let cids: CID[]
+    let cidList: CID[]
     let testCIDs = [
       testCID,
       'QmYp9d8BC2HhDCUVH7JEUZAd6Hbxrc5wBRfUs8TqazJJP9',
@@ -93,19 +110,24 @@ describe('FissionUser', () => {
     beforeAll(async () => {
       fake = sinon.fake.returns(new Promise(r => r({ data: testCIDs })))
       sinon.replace(axios, 'get', fake)
-      cids = await fission.cids()
+      cidList = await fission.cids()
+      await cids(auth)
     })
 
     it('returns list of CIDs', () => {
-      expect(cids).toEqual(testCIDs)
+      expect(cidList).toEqual(testCIDs)
     })
 
-    it('sends one GET request', () => {
-      expect(fake.callCount).toEqual(1)
+    it('sends one GET request per call', () => {
+      expect(fake.callCount).toEqual(2)
     })
 
     it('sends request with a properly formatted url', () => {
       expect(fake.args[0][0]).toEqual(`${baseURL}/ipfs/cids`)
+    })
+
+    it('defaults baseURL to https://hostless.dev', () => {
+      expect(fake.args[1][0]).toEqual(`https://hostless.dev/ipfs/cids`)
     })
   })
 
@@ -117,14 +139,15 @@ describe('FissionUser', () => {
       fake = sinon.fake.returns(new Promise(r => r({ data: testCID })))
       sinon.replace(axios, 'post', fake)
       cid = await fission.add(strContent)
+      await add(strContent, auth)
     })
 
     it('returns valid CID', () => {
       expect(cid).toEqual(testCID)
     })
 
-    it('sends one POST request', () => {
-      expect(fake.callCount).toEqual(1)
+    it('sends one POST request per call', () => {
+      expect(fake.callCount).toEqual(2)
     })
 
     it('sends request with a properly formatted url', () => {
@@ -146,6 +169,10 @@ describe('FissionUser', () => {
         }
       })
     })
+
+    it('defaults baseURL to https://hostless.dev', () => {
+      expect(fake.args[1][0]).toEqual(`https://hostless.dev/ipfs`)
+    })
   })
 
   describe('adds json (with name) to ipfs', () => {
@@ -157,14 +184,15 @@ describe('FissionUser', () => {
       fake = sinon.fake.returns(new Promise(r => r({ data: testCID })))
       sinon.replace(axios, 'post', fake)
       cid = await fission.add(jsonContent, name)
+      await add(jsonContent, auth)
     })
 
     it('returns valid CID', () => {
       expect(cid).toEqual(testCID)
     })
 
-    it('sends one POST request', () => {
-      expect(fake.callCount).toEqual(1)
+    it('sends one POST request per call', () => {
+      expect(fake.callCount).toEqual(2)
     })
 
     it('sends request with a properly formatted url', () => {
@@ -186,6 +214,10 @@ describe('FissionUser', () => {
         }
       })
     })
+
+    it('defaults baseURL to https://hostless.dev', () => {
+      expect(fake.args[1][0]).toEqual(`https://hostless.dev/ipfs`)
+    })
   })
 
   describe('pins content to ipfs', () => {
@@ -195,14 +227,19 @@ describe('FissionUser', () => {
       fake = sinon.fake.returns(new Promise(r => r({ data: {} })))
       sinon.replace(axios, 'put', fake)
       await fission.pin(testCID)
+      await pin(testCID, auth)
     })
 
-    it('sends one PUT request', () => {
-      expect(fake.callCount).toEqual(1)
+    it('sends one PUT request per call', () => {
+      expect(fake.callCount).toEqual(2)
     })
 
     it('sends request with a properly formatted url', () => {
       expect(fake.args[0][0]).toEqual(`${baseURL}/ipfs/${testCID}`)
+    })
+
+    it('defaults baseURL to https://hostless.dev', () => {
+      expect(fake.args[1][0]).toEqual(`https://hostless.dev/ipfs/${testCID}`)
     })
   })
 
@@ -213,14 +250,19 @@ describe('FissionUser', () => {
       fake = sinon.fake.returns(new Promise(r => r({ data: {} })))
       sinon.replace(axios, 'delete', fake)
       await fission.remove(testCID)
+      await remove(testCID, auth)
     })
 
-    it('sends one DELETE request', () => {
-      expect(fake.callCount).toEqual(1)
+    it('sends one DELETE request per call', () => {
+      expect(fake.callCount).toEqual(2)
     })
 
     it('sends request with a properly formatted url', () => {
       expect(fake.args[0][0]).toEqual(`${baseURL}/ipfs/${testCID}`)
+    })
+
+    it('defaults baseURL to https://hostless.dev', () => {
+      expect(fake.args[1][0]).toEqual(`https://hostless.dev/ipfs/${testCID}`)
     })
   })
 })
