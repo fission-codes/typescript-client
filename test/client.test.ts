1a5c2727647376711bf6db9b27015a165ef9b5d6
Ben Church <ben.douglas.church@gmail.com>
2019-12-02 16:51:05 -0700

    Create test helper file

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,9 +1,9 @@
+import { Object as JSONObject } from 'json-typescript'
+
 import Fission, { FissionUser, url } from '../src/client'
 import { Auth } from '../src/types'
 import { BASE_URL_DEFAULT } from '../src/constants'
-
-import { Object as JSONObject } from 'json-typescript'
-import axios from 'axios'
+import { describeRequest } from './helpers'
 
 // NOTE: We must import sinon in this manner: https://github.com/sinonjs/sinon/issues/1711
 const sinon = require('sinon')
@@ -30,49 +30,6 @@ const JSON_CONTENT = {
   string: 'testing',
   array: [1, -1, 1000, 0]
 } as JSONObject
-
-/* Helpers */
-
-type RequestTest = {
-  desc: string
-  responseData: object
-  requestFn: () => Promise<any>
-  expectedReturn: any
-  expectedUrl: string
-  expectedArguments: Array<any>
-  method: string
-}
-
-const describeRequest = (opts: RequestTest) => {
-  return describe(opts.desc, () => {
-    let response: any
-    let fake: sinon.SinonSpy
-
-    beforeAll(async () => {
-      fake = sinon.fake.returns(new Promise(r => r(opts.responseData)))
-      sinon.replace(axios, opts.method, fake)
-      response = await opts.requestFn()
-    })
-
-    it('returns expected response', () => {
-      expect(response).toEqual(opts.expectedReturn)
-    })
-
-    it('sends only one request', () => {
-      expect(fake.callCount).toEqual(1)
-    })
-
-    it('sends request with a properly formatted url', () => {
-      expect(fake.args[0][0]).toEqual(opts.expectedUrl)
-    })
-
-    it('has expected arguments', () => {
-      opts.expectedArguments.map((arg, index) => {
-        expect(fake.args[0][index + 1]).toEqual(arg)
-      })
-    })
-  })
-}
 
 /* Tests */
 
